0b043112647ab149f6a0499a76147e8df126a7bc
faye092 <liaofei167@gmail.com>
2024-06-24 15:00:08 +0200

    Update _layout.tsx
    modify the Login Screen UI, then can fully display the interface.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,9 @@
 import React from 'react';
-import { Tabs } from 'expo-router';
-import Colors from '@/constants/Colors';
+import Colors from '@/constants/Colors';  
+import TabHomeScreen from '.';
 
 const TabLayout = () => {
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors.PRIMARY,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          // Add other options like icons
-        }}
-      />
-      {/* Add other tab screens */}
-    </Tabs>
-  );
+  return <TabHomeScreen />;
 };
 
 export default TabLayout;
